121319cd6699dd9f987d52d8d9f8f798a443589d
“Ivan” <vlasoff.ivan95@gmail.com>
2022-09-18 18:17:53 +0300

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+"use strict";
+//Создайте функцию, которая принимает неограниченное количество входящих данных, и возвращает количество строк, переданных ей в этом массиве. 
+//Решить с использованием стрелочных функций.
+const calculateStr = (...num) => {
+    let result = 0;
+    for (let i = 0; i < num.length; i++) {
+        if (typeof (num[i]) === 'string') {
+            result += 1;
+        };
+    };
+    return result;
+};
+
+//Создайте функцию продвинутого калькулятора. Он принимает знак, а  затем неограниченное количество чисел, над которыми проведет указанную операцию. 
+const calcVar = (operator, ...num) => {
+    let result;
+    if (operator === '+') {
+        result = num.reduce((res, currentNumber) => res + currentNumber);
+    }
+    else if (operator === '-') {
+        result = num.reduce((res, currentNumber) => res + currentNumber);
+    }
+    else if (operator === '*') {
+        result = num.reduce((res, currentNumber) => res * currentNumber);
+    }
+    else {
+        let zeroInArray = num.findIndex(num => num === 0);
+        if (zeroInArray > 0) {
+            result = 'На ноль делить нельзя!'
+        } else {
+            result = num.reduce((res, currentNumber) => res / currentNumber);
+        }
+    };
+    return result;
+};
+
+//Имея массив чисел, найдите в нем самое большое. Не используйте циклы или методы перебора массива.
+const maxVarInArray = (...array) => {
+    return Math.max(...array);
+};
+
